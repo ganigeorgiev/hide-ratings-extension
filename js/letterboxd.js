39922ea6457e7e75fc1bf1df4cfa54ba3c9f81b1
Gani Georgiev <gani.georgiev@gmail.com>
2019-05-29 19:08:02 +0300

    [#2] added letterboxd support (+ small refactoring)

---
+++ js/letterboxd.js
@@ -0,0 +1,17 @@
+/**
+ * ===================================================================
+ * JS file that is intented to be used only in Letterboxd.
+ *
+ * @author Gani Georgiev <gani.georgiev@gmail.com>
+ * ===================================================================
+ */
+
+// Check initial state from chrome.storage
+chrome.storage.sync.get(['letterboxd'], function(items) {
+    showRatings(items.letterboxd);
+});
+
+// Listen for changes
+chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
+    showRatings(msg.letterboxd);
+});
--- js/options.js
+++ js/options.js
@@ -1,10 +1,12 @@
 ;(function () {
-    var statusVisibilityTimeout = null;
+    var statusVisibilityTimeoutId = null;
 
-    var defaultValues = {
-        imdb:      false,
-        mal:       false,
-        goodreads: false
+    // List with available rating sources and their default toggle values
+    var sourcesDefault = {
+        'imdb':       false,
+        'mal':        false,
+        'goodreads':  false,
+        'letterboxd': false
     };
 
     // Checks if chrome storage is defined
@@ -18,33 +20,30 @@
 
     // Saves toggle values in chrome.storage
     function saveToggleValues() {
-        var imdb      = !document.getElementById('imdb_toggle').classList.contains('active');
-        var mal       = !document.getElementById('mal_toggle').classList.contains('active');
-        var goodreads = !document.getElementById('goodreads_toggle').classList.contains('active');
-
         if (!hasChromeStorage()) {
             console.warn('Chrome storage is not available.')
             return;
         }
 
-        chrome.storage.sync.set({
-            imdb:      imdb,
-            mal:       mal,
-            goodreads: goodreads
-        }, function () {
+        var data = {};
+        for (let name in sourcesDefault) {
+            data[name] = !document.getElementById(name + '_toggle').classList.contains('active');
+        }
+
+        chrome.storage.sync.set(data, function () {
             chrome.tabs.getSelected(function (tab) {
                 if (tab && tab.url) {
-                    chrome.tabs.sendMessage(tab.id, {'imdb': imdb, 'mal': mal, 'goodreads': goodreads});
+                    chrome.tabs.sendMessage(tab.id, data);
                 }
             });
 
             // show status box
             var statusBox = document.getElementById('status_box');
             statusBox.classList.remove('hidden');
-            if (statusVisibilityTimeout) {
-                clearTimeout(statusVisibilityTimeout);
+            if (statusVisibilityTimeoutId) {
+                clearTimeout(statusVisibilityTimeoutId);
             }
-            statusVisibilityTimeout = setTimeout(function () {
+            statusVisibilityTimeoutId = setTimeout(function () {
                 statusBox.classList.add('hidden');
             }, 1000);
         });
@@ -52,7 +51,7 @@
 
     // Restores toggle values from chrome.storage
     function restoreToggleValues() {
-        var keys = ['imdb', 'mal', 'goodreads'];
+        var keys = Object.keys(sourcesDefault);
 
         if (!hasChromeStorage()) {
             console.warn('Chrome storage is not available.')
@@ -60,7 +59,7 @@
         }
 
         chrome.storage.sync.get(keys, function (items) {
-            var values = Object.assign({}, defaultValues, items);
+            var values = Object.assign({}, sourcesDefault, items);
 
             for (let i in keys) {
                 let key = keys[i];
@@ -85,20 +84,12 @@
         saveToggleValues();
     }
 
-    document.getElementById('goodreads_toggle').addEventListener('click', function (e) {
-        e.preventDefault();
-        toggle(this);
-     }, false);
-
-    document.getElementById('imdb_toggle').addEventListener('click', function (e) {
-        e.preventDefault();
-        toggle(this);
-     }, false);
-
-    document.getElementById('mal_toggle').addEventListener('click', function (e) {
-        e.preventDefault();
-        toggle(this);
-     }, false);
+    for (let name in sourcesDefault) {
+        document.getElementById(name + '_toggle').addEventListener('click', function (e) {
+            e.preventDefault();
+            toggle(this);
+         }, false);
+    }
 
     document.addEventListener('DOMContentLoaded', restoreToggleValues);
 })();
